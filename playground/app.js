b022a39b730705e23b5e41e68d346a4a9f3a2fad
carlos <carl.devfs@gmail.com>
2022-08-17 16:24:40 +0200

    refactor js

--- playground/app.js
+++ playground/app.js
@@ -1,19 +1,11 @@
 const body = document.querySelector("body");
-const main = document.querySelector("#main");
+const mainHtml = document.querySelector("#main").innerHTML;
 
-const ref = document.createElement("div");
-ref.classList.add("ref", "canvas");
+const classes = ["diff", "reff", "overlay"];
 
-const diff = document.createElement("div");
-diff.classList.add("diff", "canvas");
-
-const overlay = document.createElement("div");
-overlay.classList.add("overlay", "canvas");
-
-body.appendChild(ref);
-body.appendChild(diff);
-body.appendChild(overlay);
-
-ref.innerHTML = main.innerHTML;
-diff.innerHTML = main.innerHTML;
-overlay.innerHTML = main.innerHTML;
+classes.forEach((className) => {
+  let canvas = document.createElement("div");
+  canvas.classList.add("canvas", className);
+  canvas.innerHTML = mainHtml;
+  body.appendChild(canvas);
+});
