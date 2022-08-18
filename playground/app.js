36fb68ea6cafd2c15ac29a1e4a159d35048406d8
carlos <carl.devfs@gmail.com>
2022-08-18 13:48:16 +0200

    add url input to the playgound

--- playground/app.js
+++ playground/app.js
@@ -1,5 +1,6 @@
-const body = document.querySelector("body");
+const body = document.querySelector("main");
 const mainHtml = document.querySelector("#main").innerHTML;
+const btn = document.querySelector("#btn");
 
 const classes = ["diff", "reff", "overlay"];
 
@@ -8,4 +9,14 @@ classes.forEach((className) => {
   canvas.classList.add("canvas", className);
   canvas.innerHTML = mainHtml;
   body.appendChild(canvas);
+});
+
+function upadateimage() {
+  const value = document.querySelector("#url").value;
+  document.documentElement.style.setProperty("--image", "url('" + value + "')");
+}
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  upadateimage();
 });
